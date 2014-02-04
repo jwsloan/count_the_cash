1d085b2a17eed2589cb900e14381bcd6cc87bf44
John Sloan <jwsloan@gmail.com>
2014-02-03 20:44:50 -0600

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CountTheCash::Application.config.secret_key_base = '12a5e2809fea7109bc826ece4ea2706d973501e6e71b30e790ab90cb3dd1576668c02384862aa5c20ba567befbf5d9c3761875f7f930be897ad6670c5c277a7d'
