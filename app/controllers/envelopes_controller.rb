0e8bb002f4aac0850909a5adb4ebca447c5e506d
John Sloan <jwsloan@gmail.com>
2014-02-07 16:04:34 -0600

    Add edit action and do some refactoring

--- app/controllers/envelopes_controller.rb
+++ app/controllers/envelopes_controller.rb
@@ -5,12 +5,37 @@ class EnvelopesController < ApplicationController
 
   def new;
     @envelope = Envelope.new
+    render :show_form
+  end
+
+  def edit
+    @envelope = Envelope.find(params[:id])
+    render :show_form
+  end
+
+  def update
+    @envelope = Envelope.find(params[:id])
+    @envelope.update_attributes(envelope_params)
+    determine_denominations
+    @envelopes = Envelope.all
+    render :hide_form
   end
 
   def create
     @envelope = Envelope.create(envelope_params)
+    determine_denominations
     @envelopes = Envelope.all
+    render :hide_form
+  end
+
+  private
+  def envelope_params
+    params.require(:envelope).permit(:category, :name, :current_amount, :additional_amount)
+  end
+
+  def determine_denominations
     #TODO: Do math do figure out denominations
+    @envelope.denominations.destroy_all
     remaining = @envelope.additional_amount
     while remaining > 0 do
       if remaining >= 20
@@ -28,10 +53,5 @@ class EnvelopesController < ApplicationController
       end 
     end
     @envelope.save
-  end
-
-  private
-  def envelope_params
-    params.require(:envelope).permit(:category, :name, :current_amount, :additional_amount)
   end
 end
