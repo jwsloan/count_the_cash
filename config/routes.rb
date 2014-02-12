0c7122c52ea444dea5d685afdcc2283cc4f498d8
John Sloan <jwsloan@gmail.com>
2014-02-12 10:18:38 -0600

    Add modal sign-in page. Change nav link to sign-in. Redirect new user to sign-in.

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ CountTheCash::Application.routes.draw do
   root to: "envelopes#show"
   resources :envelopes, except: [:index]
   resources :users, except: [:index]
+  resources :sessions, only: [:new, :create, :destroy]
 end
