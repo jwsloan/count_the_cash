6965a1cd00bbe94d59a4006e81d08ce653e7a7b7
John Sloan <jwsloan@gmail.com>
2014-03-11 15:29:58 -0500

    Successfully creating a user in user_pages_spec.

--- spec/features/user_pages_spec.rb
+++ spec/features/user_pages_spec.rb
@@ -1,39 +1,56 @@
 require "spec_helper"
 
 feature "UserPages" do
-  
-  it "should say 'Sign up" do
+  subject { page }
+  before :each do 
     visit root_path
-    expect(page).to have_content("sign-up")
-  end
-  it "should have a sign in link", :js => true do 
-    visit root_path 
-    puts page.body
+  end 
+
+  let(:submit) { "Save" }
+
+  it { should have_content "sign-up"  }
+
+  it { should have_link "Sign in" }
+
+  scenario "clicking Sign-in provides a sign up link", :js => true do
     click_link "Sign in"
-#    click_link "Sign up now!"
+    expect(page).to have_link "Sign up now!"      
+  end
+
 #    fill_in :email, :with => user.email
 #    fill_in :name, :with => user.name
 
 #    Capybara.save_and_open_page
-#    let(:submit) {"Save" }
   
-#    describe "with invalid information" do 
-#      it "should not create a user" do
-#        expect { click_button submit }.not_to change(User, :count)
-#      end
-#    end
+#   describe "with invalid information" do 
+#     it "should not create a user" do
+#       expect { click_button submit }.not_to change(User, :count)
+#     end
+#   end
 
-#    describe "with valid information" do
-#      before do
-#        fill_in "Name",         :with => "Example"
-#        fill_in "Email",        :with => "user@example.com"
-#        fill_in "user_password",     :with => "password"
-#        fill_in "user_password_confirmation",     :with => "password"
-###      end
-      
-#      it "should create a user" do
-#        expect { click_button submit }.to change(User, :count).by(1)
-#      end
-#    end
-  end
+    describe "with valid information", :js => true do
+      before do
+        click_link "Sign in"
+        click_link "Sign up now!"
+
+        fill_in "Name",         :with => "Example"
+        fill_in "Email",        :with => "user@example.com"
+        fill_in "user_password",     :with => "password"
+        fill_in "user_password_confirmation",     :with => "password"
+      end
+
+      it "should have a filled-in form" do
+        find_field("Name").value.should eq "Example"
+        find_field("Email").value.should eq "user@example.com"
+        find_field("user_password").value.should eq "password"
+        find_field("user_password_confirmation").value.should eq "password"
+      end
+
+     it "should create a user" do
+       #expect { click_button submit }.to change(User, :count).by(1)
+       click_button submit
+       expect(page).to have_link("New Envelope")
+     end
+    end
+  #end
 end
